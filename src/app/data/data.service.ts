730fa12c232566cb44b92ee07e438cc20f9929ea
Mary T. Welch <Mary.Welch@artisantechgroup.com>
2021-01-03 18:57:04 -0600

    mocking response in putsreq.com

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -11,8 +11,15 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   postUserSettingsForm(userSettings: UserSettings) : Observable<any> {
-    
-    return this.http.post('url', userSettings);
+    /*
+      Response builder in putsreq.com:
+        var parsedBody = JSON.parse(request.body);
+        parsedBody.id = '1234';
+        response.body = parsedBody;
+    */
+
+
+    return this.http.post('https://putsreq.com/PoaEI8wMsxgKufm7nuoJ', userSettings);
     
     //return of(userSettings);
   }
